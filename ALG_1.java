08c85f7fae9e653459c3aae2820c24d1a8a7e249
jemimahab22-blip <jemimahab22@gmail.com>
2026-09-23 18:24:23 -0600

    Algorithm 1

--- ALG_1.java
+++ ALG_1.java
@@ -1,6 +1,11 @@
 public class ALG_1 {
     public static int fibonacci(int n) {
         // TODO: Implement Algorithm 1 Recursive Fibonacci Calculation
-        return 0;
+        if(n==0)return 0;
+        if(n==1)return 1;
+        if( n>=2){
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+        return -1;
     }
 }
